75c4b16d6ea277cbeb8230ed95e7958e218bdd76
sotossotos <sotossotos31@yahoo.com>
2021-06-03 12:50:42 +0100

    Added joi schema for customer-table

---
+++ src/handlers/createStrictCustomer.js
@@ -0,0 +1,27 @@
+const Responses = require('../utils/API_Responses');
+const Dynamo = require('../utils/Dynamo');
+const customerSchema= require('../../tables/customer-schema');
+
+const tableName = process.env.tableName;
+exports.handler = async event => {
+
+    let ID = event.pathParameters.ID;
+    const customer = JSON.parse(event.body);
+    customer.ID = ID;
+    //res has value and error fields
+    const res=customerSchema.validate(customer);
+    if(typeof(res.error)!== 'undefined'){
+        const errorMsg=res.error.message.replace(/"/g,'');
+        return Responses._400({ message: 'ERROR: '+errorMsg });
+    }
+    const newCustomer = await Dynamo.write(customer, tableName).catch(err => {
+        console.log('error in dynamo write', err);
+        return null;
+    });
+
+    if (!newCustomer) {
+        return Responses._400({ message: 'Failed to write customer by ID' });
+    }
+
+    return Responses._200({ newCustomer });
+};
