d975a364330d61104a4460eaf34d9f9fb89de909
sotossotos <sotossotos31@yahoo.com>
2021-06-03 16:26:25 +0100

    Fixed pull request issues and added 500 error handling and unit test for validation

--- tables/customer-schema.js
+++ tables/customer-schema.js
@@ -5,6 +5,6 @@ const customerSchema=joi.object().keys({
     ID:joi.string().alphanum().min(5).required(),
     name:joi.string().required(),
     surname:joi.string(),
-    birth_year:joi.number().integer().min(1900).max(2009).required()
+    birthYear:joi.number().integer().min(1900).max(2009).required()
 });
 module.exports=customerSchema;
---
+++ tables/customer-schema.unit.test.js
@@ -0,0 +1,32 @@
+const customerSchema = require('../tables/customer-schema');
+
+test('Customer Schema is an object', ()=>{
+    expect(typeof customerSchema).toBe('object');
+});
+
+test('Customer Schema sanity check', ()=>{
+    data={ID:"12345",name:"someName",surname:"someSurname",birthYear:"2001"};
+    const res= customerSchema.validate(data);
+    expect(res.error).toStrictEqual(undefined);
+});
+test('Customer Schema sanity check missing surname', ()=>{
+    data={ID:"12345",name:"someName",birthYear:"2001"};
+    const res= customerSchema.validate(data);
+    expect(res.error).toStrictEqual(undefined);
+});
+
+test('Customer Schema age problem', ()=>{
+    data={ID:"12345",name:"someName",surname:"someSurname",birthYear:"1899"};
+    const res= customerSchema.validate(data);
+    expect(res.error.details[0].type).toBe("number.min");
+});
+test('Customer Schema age problem', ()=>{
+    data={ID:"12345",name:"someName",surname:"someSurname",birthYear:"2010"};
+    const res= customerSchema.validate(data);
+    expect(res.error.details[0].type).toBe("number.max");
+});
+test('Customer Schema ID LENGTH', ()=>{
+    data={ID:"12",name:"someName",surname:"someSurname",birthYear:"2010"};
+    const res= customerSchema.validate(data);
+    expect(res.error.details[0].type).toBe("string.min");
+});
