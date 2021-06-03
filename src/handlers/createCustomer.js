0c5e6266fef5127a7e771739a981934b58a0a7b8
sotossotos <sotossotos31@yahoo.com>
2021-06-03 10:26:33 +0100

    table environment

--- src/handlers/createCustomer.js
+++ src/handlers/createCustomer.js
@@ -1,8 +1,8 @@
 const Responses = require('../utils/API_Responses');
 const Dynamo = require('../utils/Dynamo');
 
-const tableName = process.env.tableName;
-//const tableName='customer-table';
+//const tableName = process.env.tableName;
+const tableName='customer-table';
 exports.handler = async event => {
     
     if (!event.pathParameters || !event.pathParameters.ID) {
@@ -15,8 +15,8 @@ exports.handler = async event => {
         return Responses._400({ message: 'JSON attr missing:name,surname' });
     }
     customer.ID = ID;
-
-    const newCustomer = await Dynamo.write(customer, process.env.tableName).catch(err => {
+    // Changed tableName -> process.env.tableName
+    const newCustomer = await Dynamo.write(customer, tableName).catch(err => {
         console.log('error in dynamo write', err);
         return null;
     });
