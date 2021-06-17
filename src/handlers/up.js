6f527de9388ec69d5511b531d2cd2082f2d75180
sotossotos <sotossotos31@yahoo.com>
2021-06-17 15:57:03 +0100

    Adding sequelize lib, changed somehtings in webpack to work with pg error

--- src/handlers/up.js
+++ src/handlers/up.js
@@ -1,13 +1,28 @@
 
 // First try of creating lambda
-
+import * as pg from 'pg';
+import { Sequelize } from 'sequelize'
 import Responses from '../utils/API_Responses.js';
 /**
  * 
  * @param {import('serverless/plugins/aws/package/compile/events/apiGateway/lib/validate').ApiGatewayEvent} event 
  * @returns {JSON}
  */
+const sequelize = new Sequelize('customers','user','password',{
+  host:'localhost',
+  dialect:'postgres',
+  dialectModule:pg,
+})
 exports.handler = async (event) => {
+
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established Postgres DB successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
   return Responses._200({ statusCode: 'success', message: 'AnotherOne is Running !', input: event });
+
+
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -3,4 +3,12 @@ module.exports = {
     target: 'node',
     mode: 'none',
     entry: slsw.lib.entries,
+    resolve: {
+      alias: {
+        'pg-native': 'noop2',
+        tedious: 'noop2',
+        sqlite3: 'noop2',
+        mysql2: 'noop2',
+      },
+    },
 };
